dda4cb3ae4a5193b654335653a34ba815e61f125
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-28 10:58:35 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Khumbongmayum Tonny on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
